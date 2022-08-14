1177d5f8cbad607930166eb56f102a3b7d0b57d4
cesardramirez <cesardavid89@gmail.com>
2022-08-13 21:36:04 -0500

    feat(variables)

---
+++ 02_Variables.js
@@ -0,0 +1,22 @@
+// Los valores se guardan en las variables (espacio en memoria).
+// El ; se utiliza al final de cada línea como buena práctica.
+
+/*
+  var : Creación de una variable.
+*/
+var name = "David"
+
+var age   // Declarar.
+age       // undefined
+age = 30  // Inicializar.
+
+var elements = ["Computadora", "Celular"]  // string[]
+elements.length  // 2
+elements[1]  // 'Celular'
+
+var person = {
+    name: "Pedro",
+    age: 30
+}
+person.name  // 'Pedro'
+person.age  // 30
