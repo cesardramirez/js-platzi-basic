dc9440549450dd81d3b8e3a1e71409343a3c0b29
cesardramirez <cesardavid89@gmail.com>
2022-09-03 18:48:18 -0500

    feat(operators)

---
+++ 08_Operators.js
@@ -0,0 +1,30 @@
+// Operadores Aritméticos.
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+
+// Concatenación de Strings
+"Cesar " + "Ramírez"
+
+// Negación
+!false  // true
+
+// Asignación
+var a = 1
+
+// Comparación
+3 == "3"   // true : No compara el tipo de dato, sino el valor.
+3 === "3"  // false : Compara tanto el tipo de dato como el valor.
+5 > 3
+5 < 3
+4 <= 8
+5 >= 6
+// Comparadores lógicos
+a && b  // Las dos condiciones deben ser verdaderas.
+a || b  // Alguna de las dos condiciones debe ser verdadera.
+
+// Incremento
+var age = 40
+age++  // 40 : Cuando se vuelva a llamar tendrá el valor 41.
+age += 3 // 44 : A la variable actual le suma 3.
