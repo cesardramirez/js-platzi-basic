c7715e51094fbe3cccf79e47132511e4e7bb87ed
cesardramirez <cesardavid89@gmail.com>
2022-09-03 18:33:56 -0500

    feat(truthy|falsy)

---
+++ 07_TruthyFalsy.js
@@ -0,0 +1,14 @@
+Boolean()   // false : Valor por defecto.
+Boolean(0)  // false
+Boolean(1)  // true
+Boolean(null)  // false
+Boolean(NaN)  // false : NaN : Not-A-Number
+Boolean(undefined)  // false
+Boolean(false)  // false
+Boolean(true)  // true
+Boolean("")  // false
+Boolean(" ")  // true : Importante limpiar los espacios.
+Boolean("a")  // true
+Boolean([])  // true
+Boolean({})  // true
+Boolean(function(){})  // true
